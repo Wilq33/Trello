6adb9a5b2024f1e3037a7d3617a95922f2bf9d5c
Bartlomiej Wilk <34482885+Wilq33@users.noreply.github.com>
2019-11-07 19:58:53 +0100

    Adding if/else, correcting the steps

--- src/Test1.java
+++ src/Test1.java
@@ -3,13 +3,15 @@ import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
 public class Test1 {
 
     private WebDriver driver;
     private String trello;
     private String email;
     private String signup;
+    private String trelloError;
+    private String name;
+    private WebElement element;
 
 
     @Before
@@ -18,28 +20,51 @@ public class Test1 {
         trello = "https://trello.com/signup";
         email = "email";
         signup = "signup";
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        trelloError = "email-error";
+        name = "name";
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testSeleniumTestForm() {
+    public void testForm() {
 
         driver.get(trello);
         driver.findElement(By.id(email)).clear();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
         driver.findElement(By.id(email)).sendKeys("test@test.pl");
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(By.id(signup));
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
+        driver.findElement(By.id(signup)).click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
         driver.findElement(By.id(email)).clear();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
         driver.findElement(By.id(email)).sendKeys("test");
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
+        driver.findElement(By.id(name)).click();
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 
-        }
+        try {
+
+            element = driver.findElement(By.id(trelloError));
+
+
+            if(element.isDisplayed()){
+
+                System.out.println("I can see the message");
+
+            }else{
+
+                System.out.println("I cannot see the message");
+            }
+
+            /*System.out.println(trelloError);*/
+            /*public static void assertEquals("That doesn't look like an email address…", trelloError);*/
+        }catch (Exception e){};
+    }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
 
         driver.quit();
 
     }
-
 }
